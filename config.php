680e942d4716e5ca2303c9e0a0b95b71f8d66070
snowly1 <domextosz@gmail.com>
2018-11-08 20:55:34 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['db']['host'] = 'localhost';
+$config['db']['port'] = '3306';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'realsnowly5588';
+$config['db']['dbname'] = 'forum-xenforo';
+
+$config['fullUnicode'] = true;
